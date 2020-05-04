3d7b905d430a384d6a59f033f83c83674eb89db8
Marcella <marcellatsangos@gmail.com>
2020-05-04 10:57:11 -0300

    Functions for adding digit and clearing calculator memory

--- calculator/src/main/Calculator.jsx
+++ calculator/src/main/Calculator.jsx
@@ -5,7 +5,17 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+  displayValue: '0', // valor exibido no display da calculadora
+  clearDisplay: false, // se precisa ou não limpar o display
+  operation: null, // armazena a operação
+  values: [0, 0], // valores digitados (antes e depois da operação)
+  current: 0 // verifica se está manipulando o valor de índice 0 ou 1
+}
+
 export default class Calculator extends Component {
+  state = {...initialState}
+
   constructor(props) {
     super(props)
     this.clearMemory = this.clearMemory.bind(this)
@@ -13,8 +23,9 @@ export default class Calculator extends Component {
     this.addDigit = this.addDigit.bind(this)
   }
 
-  // Zera a memória da calculadora.
+  // Zera a memória da calculadora e restaura o estado inicial.
   clearMemory() {
+    this.setState({ ...initialState })
     console.log('Memória zerada')
   }
 
@@ -25,13 +36,29 @@ export default class Calculator extends Component {
 
   // Adiciona um dígito.
   addDigit(n) {
-    console.log(`Digitado: ${n}`)
+    // console.log(`Digitado: ${n}`)
+    if (n === '.' && this.state.displayValue.includes('.')) return
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay // limpa se o dígito for 0 ou a variável for true
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    // Pega o índice do valor que está alterando, converte para float, clona, altera o valor atual e altera o estado.
+    if (n !== '.') {
+      const i = this.state.current
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[i] = newValue
+      this.setState({ values })
+      console.log(values)
+    }
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={100} />
+        <Display value={this.state.displayValue} />
 
         <Button label="AC" click={this.clearMemory} triple />
         <Button label="/" click={this.setOperation} operation />
