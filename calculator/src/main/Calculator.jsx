4d22b8e673c55af2c79ad0132dd7f4eda2208e88
Marcella <marcellatsangos@gmail.com>
2020-05-04 11:11:59 -0300

    Operations function

--- calculator/src/main/Calculator.jsx
+++ calculator/src/main/Calculator.jsx
@@ -29,14 +29,37 @@ export default class Calculator extends Component {
     console.log('Memória zerada')
   }
 
-  // Executa operação escolhida.
+  // Executa a operação escolhida.
   setOperation(operation) {
-    console.log(`A operação escolhida foi ${operation}`)
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const currentOperation = this.state.operation // quando já possuir uma operação setada
+      
+      const values = [...this.state.values]
+
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+      } catch (error) {
+        values[0] = this.state.values[0]
+        console.log(`ERROR: ${error}`)
+      }
+
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+        values
+      })
+    }
   }
 
   // Adiciona um dígito.
   addDigit(n) {
-    // console.log(`Digitado: ${n}`)
     if (n === '.' && this.state.displayValue.includes('.')) return
 
     const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay // limpa se o dígito for 0 ou a variável for true
