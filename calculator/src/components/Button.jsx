a6ff51d92af15d4f33b3cd85c28033fbf79391ce
Marcella <marcellatsangos@gmail.com>
2020-05-04 10:09:55 -0300

    Button component

--- calculator/src/components/Button.jsx
+++ calculator/src/components/Button.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import './Buton.css'
 
-export default props =>
-  <button className="button">{props.label}</button>
+export default props => {
+  let classes = 'button '
+  classes += props.operation ? 'operation' : ''
+  classes += props.double ? 'double' : ''
+  classes += props.triple ? 'triple' : ''
+
+  return (
+    <button 
+      onClick={e => props.click && props.click(props.label)} // recebe o conteúdo do que for clicado
+      className={classes}>
+      {props.label}
+    </button>
+  )
+}
